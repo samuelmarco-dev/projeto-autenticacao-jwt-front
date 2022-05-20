ada6cb7e2d16007d7c5969dd91b8b7ffc79b1c97
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-05-20 00:00:21 -0300

    fix: correção de rotas com Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Router, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import TelaCadastro from './TelaCadastro';
 import TelaGaleria from './TelaGaleria';
@@ -10,12 +10,12 @@ import '../assets/css/reset.css';
 function App() {
     return (  
         <BrowserRouter>
-            <Router>
+            <Routes>
                 <Route path='/sign-in' element={<TelaInicial/> }/>
                 <Route path='/sign-up' element={<TelaCadastro/> }/>
                 <Route path='/' element={<TelaGaleria/> }/>
                 <Route path='/image' element={<TelaImage/> }/>
-            </Router>
+            </Routes>
         </BrowserRouter>
     );
 }
